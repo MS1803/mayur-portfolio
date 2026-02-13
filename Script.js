e0260c945aa41846b2a1d416416b49d68a1c75d5
Mayur Sonawane <mayursonawane922@gmail.com>
2026-02-14 01:39:04 +0530

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,8 @@
+// smooth scrolling
+document.querySelectorAll('a').forEach(anchor=>{
+    anchor.addEventListener('click', function(e){
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href'))
+        .scrollIntoView({behavior:'smooth'});
+    });
+});
